bd9c585b661f2e124f5f49066710e07717e3b265
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-02-07 23:05:30 -0300

    Product Crud Completed

--- club_77/club_77/urls.py
+++ club_77/club_77/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('detalhes/<int:id>/', views.detalhes_produto, name='detalhes_produto'),
     path('adicionar/', views.adicionar_produto, name='adicionar_produto'),
     path('detalhes/<int:produto_id>/', views.detalhes_produto, name='detalhes_produto'),
+    path('editar/<int:produto_id>/', views.editar_produto, name='editar_produto'),
+    path('confirmar_exclusao/<int:produto_id>/', views.confirmar_exclusao, name='confirmar_exclusao'),
+    path('deletar/<int:produto_id>/', views.deletar_produto, name='deletar_produto'),
 ]
--- club_77/produtos/views.py
+++ club_77/produtos/views.py
@@ -3,7 +3,6 @@ from .models import Produto
 from .forms import ProdutoForm
 
 
-
 def lista_produtos(request):
     produtos = Produto.objects.all()
     return render(request, 'produtos/lista_produtos.html', {'produtos': produtos})
@@ -23,3 +22,30 @@ def adicionar_produto(request):
     else:
         form = ProdutoForm()
     return render(request, 'produtos/adicionar_produto.html', {'form': form})
+
+
+def editar_produto(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'produtos/editar_produto.html', {'form': form})
+
+
+def confirmar_exclusao(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('lista_produtos')
+    return render(request, 'produtos/confirmar_exclusao_produto.html', {'produto': produto})
+
+
+def deletar_produto(request, produto_id):
+    produto = get_object_or_404(Produto, pk=produto_id)
+    if request.method == 'GET':
+        return redirect('confirmar_exclusao', produto_id=produto_id)
+    return redirect('lista_produtos')
