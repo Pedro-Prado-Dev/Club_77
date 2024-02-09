f781789c96a081a7970854fb18bf4780e2c35595
Pedro-Prado-Dev <pedroppaiva1@hotmail.com>
2024-02-08 22:45:47 -0300

    Initial configuration with clients

---
+++ club_77/clientes/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_clientes, name='lista_clientes'),
+    path('adicionar/', views.adicionar_cliente, name='adicionar_cliente'),
+    path('editar/<int:cliente_id>/', views.editar_cliente, name='editar_cliente'),
+    path('deletar/<int:cliente_id>/', views.deletar_cliente, name='deletar_cliente'),
+]
